3ae18664da382e509402d82c1869e8d7b46a4eef
Dinesh Pandiyan <flexdinesh@gmail.com>
2024-02-13 23:19:25 +1100

    base setup + a few themes

--- init.lua
+++ init.lua
@@ -18,7 +18,9 @@ return {
   },
 
   -- Set colorscheme to use
-  colorscheme = "astrodark",
+  -- colorscheme = "onedark",
+  -- colorscheme = "gruvbox",
+  colorscheme = "nightfox",
 
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
---
+++ plugins/colorscheme.lua
@@ -0,0 +1,18 @@
+return {
+   {
+      "catppuccin/nvim",
+      name = "catppuccin",
+      opts = {
+        -- configuration options...
+      },
+    },
+    {
+      "navarasu/onedark.nvim",
+      name = "onedark",
+      opts = {
+        -- configuration options...
+      },
+    },
+    { "ellisonleao/gruvbox.nvim" },
+    { "EdenEast/nightfox.nvim" }
+}
